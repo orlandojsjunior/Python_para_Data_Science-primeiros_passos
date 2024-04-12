795584fc87e021bd7e815c74327b4429c5c07212
Orlando Junior <orlandojsjunior@hotmail.com>
2024-04-12 01:55:59 -0300

    Começando com o Python

---
+++ Hora_da_pratica.py
@@ -0,0 +1,30 @@
+'''
+
+Vamos praticar o uso da função print com algumas atividades. Para isso, solucione os problemas propostos em código:
+
+Imprima a frase Escola de Dados da Alura!.
+
+Imprima seu nome e seu sobrenome seguindo a estrutura abaixo:
+
+Nome: [seu nome]
+Sobrenome: [seu sobrenome]
+
+Imprima o seu primeiro nome letra a letra. Por exemplo, meu nome é Mirla, então eu obtenho a seguinte saída:
+M
+I
+R
+L
+A
+
+Imprima o dia do seu nascimento em formato dia mês ano. Lembrando que os valores de dia e ano não podem estar entre aspas. Supondo uma data de aniversário dia 28 de fevereiro de 2003, o formato deve estar como no exemplo abaixo:
+28 fevereiro 2003
+
+Imprima, em um único print, o atual ano que você está fazendo esse curso. O valor do ano deve ser um dado numérico e a saída do print deve ser a seguinte:
+Ano atual: [ano]
+
+Caso precise de ajuda, opções de solução das atividades estão disponíveis na seção “Opinião da pessoa instrutora”.
+
+OBS: Para ajudar a verificar seus códigos, disponibilizamos um notebook dos desafios para construir suas soluções. Você pode baixar ele e depois fazer o upload do notebook no Google Colab, assim como fizemos em aula. Caso queira adicionar mais células, use a sequência Ctrl M B no seu teclado para gerar novas células de código.
+
+
+'''
